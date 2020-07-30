73b7dc5788091a39782feb35a13d7afc8b4a72df
Liuchang <939257783@qq.com>
2020-07-30 18:49:34 +0800

    初始化异常监听

---
+++ src/Exception.php
@@ -0,0 +1,9 @@
+<?php
+namespace HyperfException;
+
+use Hyperf\Server\Exception\ServerException;
+
+class Exception extends ServerException
+{
+    
+}
---
+++ src/Handler.php
@@ -0,0 +1,38 @@
+<?php
+namespace HyperfException;
+
+use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    public function handle(Throwable $throwable, ResponseInterface $response)
+    {
+        // 判断被捕获到的异常是希望被捕获的异常
+        if ($throwable instanceof Exception)
+        {
+            // 格式化输出
+            $data = json_encode([
+                'code' => $throwable->getCode(),
+                'message' => $throwable->getMessage(),
+            ], JSON_UNESCAPED_UNICODE);
+
+            // 阻止异常冒泡
+            $this->stopPropagation();
+            return $response->withBody(new SwooleStream($data));
+        }
+
+        // 交给下一个异常处理器
+        return $response;
+    }
+
+    /**
+     * 判断该异常处理器是否要对该异常进行处理
+     */
+    public function isValid(Throwable $throwable): bool
+    {
+        return true;
+    }
+}
